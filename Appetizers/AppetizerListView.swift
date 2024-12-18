c0c1f76b9a075c4430293b2d0485cfe6a635583c
BryllieBrinkley <jibryllbrink@yahoo.com>
2024-12-18 16:09:46 -0500

    Set up app structure and implement TabView navigation

---
+++ Appetizers/AppetizerListView.swift
@@ -0,0 +1,21 @@
+//
+//  AppetizerListView.swift
+//  Appetizers
+//
+//  Created by Jibryll Brinkley on 12/18/24.
+//
+
+import SwiftUI
+
+struct AppetizerListView: View {
+    var body: some View {
+        NavigationView {
+            Text("Appetizer List View")
+                .navigationTitle("🥟 Appetizers")
+        }
+    }
+}
+
+#Preview {
+    AppetizerListView()
+}
