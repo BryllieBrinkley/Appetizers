3bd5a59170e30dd6df3247e214f9f8b6620fc0ec
BryllieBrinkley <jibryllbrink@yahoo.com>
2024-12-26 12:40:51 -0500

    account view UI setup

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -8,10 +8,42 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lasttName = ""
+    @State private var email = ""
+    @State private var birthdate = Date()
+    @State private var extraNapkins = false
+    @State private var noIce = false
+    
     var body: some View {
         NavigationView {
-            Text("Account View")
-                .navigationTitle("👤 Account")
+            Form {
+                Section(header: Text("Personal Info")) {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lasttName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                    DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
+                    
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save Changes")
+                    }
+                }
+                
+                Section(header: Text("Requests")) {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("No Ice", isOn: $noIce)
+                }
+                .toggleStyle(SwitchToggleStyle(tint: Color("brandPrimary")))
+            }
+            .navigationTitle("👥 Account")
+            
+
         }
     }
 }
